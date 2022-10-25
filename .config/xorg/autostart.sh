027c0e5d669b511695f4bd3ff930523d0fe65c3f
p-kolacz <piotr@kolacz.org>
2022-10-25 22:18:59 +0200

    xorg

---
+++ .config/xorg/autostart.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# "$DOTFILES/xorg/monitor-setup.sh" > "$XDG_STATE_HOME/xrandr.log"
+wallpaper restore &
+picom -b
+setxkbmap -option caps:escape
+kbrate.sh
+bluetoothctl power on &
+numlockx &
+
