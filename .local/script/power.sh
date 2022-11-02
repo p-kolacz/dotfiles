cb03ecf831ef8f042735fcffb977e5109674c625
p-kolacz <piotr@kolacz.org>
2022-11-02 16:23:04 +0100

    2022-11-02_16:23:04

--- .local/script/power.sh
+++ .local/script/power.sh
@@ -2,7 +2,7 @@
 
 case "$(echo -e "鈴 Suspend\n Lock\n Reboot\n Poweroff" | rofi -dmenu -i -p " " -theme-str 'window {width:15%;} listview {lines:4;}')" in
         "鈴 Suspend") exec systemctl suspend;;
-		" Lock") exec slock ;;
+		" Lock") exec lock-workstation ;;
         " Reboot") exec systemctl reboot;;
         " Poweroff") exec systemctl poweroff;;
 		# Hibernate) exec systemctl hibernate;;
