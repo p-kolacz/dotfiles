250f1cf19a4d8ab77ada30ce9597d5770926a299
p-kolacz <piotr@kolacz.org>
2022-12-30 10:22:18 +0100

    2022-12-30_10:22:18

--- .config/nvim/lua/tool/vim-test.lua
+++ .config/nvim/lua/tool/vim-test.lua
@@ -1,6 +1,6 @@
 Plugin "https://github.com/vim-test/vim-test"
 
-vim.opt['test#strategy'] = "neovim"
+-- vim.opt['test#strategy'] = "neovim"
 
 mapgroup("<leader>t", "+Tests")
 nnoremap("<leader>tn", ":TestNearest<cr>", "nearest")
