2e9f7769fed7edece6274bbf42f4a3a4ce962ed5
p-kolacz <piotr@kolacz.org>
2023-01-24 14:32:40 +0100

    2023-01-24_14:32:40

--- .config/nvim/lua/conf/appearance.lua
+++ .config/nvim/lua/conf/appearance.lua
@@ -3,7 +3,7 @@ set.fillchars      = Icons.fillchars
 set.cursorline     = true
 set.wrap           = false
 set.number         = true
-set.scrolloff      = 5		-- top and bottom margin in rows
+set.scrolloff      = 5
 set.sidescrolloff  = 5
 set.showmatch      = true
 set.signcolumn     = "yes"
@@ -18,11 +18,11 @@ Icons.setup_devicons()
 
 Plugin "https://github.com/RRethy/vim-illuminate"
 require "illuminate".configure {
-	filetypes_allowlist = {"lua", "rails", "ruby", "sh", },
+	filetypes_allowlist = { "lua", "rails", "ruby", "sh", },
 	min_count_to_highlight = 2,
 }
 Plugin "https://github.com/petertriho/nvim-scrollbar"
-require"scrollbar".setup{
+require"scrollbar".setup {
 	handlers = {
 		cursor = false
 	}
