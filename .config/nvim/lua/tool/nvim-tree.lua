fe32249f53a81bf626b393d08524a18e9acdaee7
p-kolacz <piotr@kolacz.org>
2023-05-21 00:46:24 +0200

    2023-05-21_00:46:24

--- .config/nvim/lua/tool/nvim-tree.lua
+++ .config/nvim/lua/tool/nvim-tree.lua
@@ -34,21 +34,38 @@ require'nvim-tree'.setup {
 	},
 	view = {
 		width = 40,
-		mappings = {
-			custom_only = false,
-			list = {
-				{ key = "l", action = "edit" },
-				{ key = "h", action = "close_node" },
-				{ key = "H", action = "collapse_all" },
-				{ key = "<C-F>", action = "grep in dir", action_cb = grep_in_dir },
-			},
-		},
+		-- mappings = {
+		-- 	custom_only = false,
+		-- 	list = {
+		-- 		{ key = "l", action = "edit" },
+		-- 		{ key = "h", action = "close_node" },
+		-- 		{ key = "H", action = "collapse_all" },
+		-- 		{ key = "<C-F>", action = "grep in dir", action_cb = grep_in_dir },
+		-- 	},
+		-- },
 	},
 	actions = {
 		open_file = {
 			resize_window = false,
 		},
 	},
+	on_attach = function(bufnr)
+		local api = require('nvim-tree.api')
+
+		local function opts(desc)
+			return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+		end
+
+		api.config.mappings.default_on_attach(bufnr)
+
+		vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
+		vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Close Directory'))
+		vim.keymap.set('n', 'H', api.tree.collapse_all, opts('Collapse'))
+		vim.keymap.set('n', '<C-F>', function()
+			local node = api.tree.get_node_under_cursor()
+			grep_in_dir(node)
+		end, opts('grep in dir'))
+	end
 }
 
 nnoremap('<F8>', ':NvimTreeToggle<cr>')
--- .local/script/rclone-mount.sh
+++ .local/script/rclone-mount.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-mkdir -p ~/server/{crypt,static,vault}
+mkdir -p ~/server/{crypt,static,vault,wiki}
 
 rclone mount --daemon --vfs-cache-mode full --log-file="$XDG_STATE_HOME/log/rclone-crypt.log" crypt: ~/server/crypt
 rclone mount --daemon --vfs-cache-mode full --log-file="$XDG_STATE_HOME/log/rclone-static.log" vault:apps/static ~/server/static
 rclone mount --daemon --vfs-cache-mode full --log-file="$XDG_STATE_HOME/log/rclone-vault.log" vault:files/vault ~/server/vault
+rclone mount --daemon --vfs-cache-mode full --log-file="$XDG_STATE_HOME/log/rclone-wiki.log" vault:apps/tiddlywiki/wiki ~/server/wiki
 
