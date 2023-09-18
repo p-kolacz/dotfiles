3e02c3fbe68c867faa1b5555fa556972520a69c4
p-kolacz <piotr@kolacz.org>
2023-09-18 16:09:54 +0200

    2023-09-18_16:09:54

--- .local/script/automount.sh
+++ .local/script/automount.sh
@@ -7,8 +7,7 @@ pathtoname() {
 stdbuf -oL -- udevadm monitor --udev -s block | while read -r -- _ _ event devpath _; do
 	if [ "$event" = add ]; then
 		devname=$(pathtoname "$devpath")
-		udisksctl mount --block-device "$devname" --no-user-interaction
-		notify-send -i "$HOME/.local/share/icons/arch.svg" "UDisks" "$devname mounted"
+		udisksctl mount --block-device "$devname" --no-user-interaction && notify-send -i "$HOME/.local/share/icons/arch.svg" "UDisks" "$devname mounted"
 	elif [ "$event" = remove ]; then
 		notify-send -i "$HOME/.local/share/icons/arch.svg" "UDisks" "$devname removed"
 	fi
