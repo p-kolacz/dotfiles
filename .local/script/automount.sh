227ea019aa0a56b1eeeb82101b95c1984e16e8a7
p-kolacz <piotr@kolacz.org>
2024-06-06 18:09:28 +0200

    2024-06-06_18:09:28

--- .local/script/automount.sh
+++ .local/script/automount.sh
@@ -6,11 +6,12 @@ pathtoname() {
 
 onmount() {
 	signal usb-mounted "$1"
-	notify-send "UDisks" "$1 mounted"
+	LABEL=$(blkid -s LABEL -o value "$1")
+	notify-send "UDisks" "$LABEL ($1) mounted"
 }
 
 onremove() {
-	notify-send "UDisks" "$devname removed"
+	notify-send "UDisks" "Disk removed"
 }
 
 stdbuf -oL -- udevadm monitor --udev -s block | while read -r -- _ _ event devpath _; do
@@ -18,7 +19,7 @@ stdbuf -oL -- udevadm monitor --udev -s block | while read -r -- _ _ event devpa
 		devname=$(pathtoname "$devpath")
 		udisksctl mount --block-device "$devname" --no-user-interaction && onmount "$devname"
 	elif [ "$event" = remove ]; then
-		onremove
+		onremove "$devpath"
 	fi
 done
 
