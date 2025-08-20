ac59104932f89b339a13252519cf8733638a16c7
PK <piotr@kolacz.org>
2025-08-21 01:11:04 +0200

    2025-08-21_01:11:04

--- .local/script/rclone-mount.sh
+++ .local/script/rclone-mount.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p ~/server/{crypt,oracle,static,vault,wiki,zima}
+mkdir -p ~/server/{crypt,elements,oracle,static,vault,wiki,zima}
 
 rclone mount --daemon --vfs-cache-mode full --log-file="$XDG_STATE_HOME/log/rclone-crypt.log" crypt: ~/server/crypt
 # rclone mount --daemon --vfs-cache-mode full --log-file="$XDG_STATE_HOME/log/rclone-oracle.log" oracle: ~/server/oracle
@@ -8,4 +8,5 @@ rclone mount --daemon --vfs-cache-mode full --log-file="$XDG_STATE_HOME/log/rclo
 rclone mount --daemon --vfs-cache-mode full --log-file="$XDG_STATE_HOME/log/rclone-vault.log" vault:files/vault ~/server/vault
 rclone mount --daemon --vfs-cache-mode full --log-file="$XDG_STATE_HOME/log/rclone-wiki.log" vault:apps/tiddlywiki/wiki ~/server/wiki
 rclone mount --daemon --vfs-cache-mode full --log-file="$XDG_STATE_HOME/log/rclone-zima.log" zima:DATA ~/server/zima
+rclone mount --daemon --vfs-cache-mode full --log-file="$XDG_STATE_HOME/log/rclone-elements.log" zima:Elements ~/server/elements
 
