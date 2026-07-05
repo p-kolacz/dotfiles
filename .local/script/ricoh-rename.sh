a389ce89c0fc55267bd928af673b3edb85d61058
PK <piotr@kolacz.org>
2026-07-05 21:01:49 +0200

    2026-07-05_21:01:49

--- .local/script/ricoh-rename.sh
+++ .local/script/ricoh-rename.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
-shopt -s nullglob
+shopt -s nullglob nocaseglob
 
-for file in R*.JPG; do
-	# Get modification time in desired format
-	timestamp=$(date -r "$file" +"%Y%m%d_%H%M")
+for file in R*.JPG R*.DNG IMG2*.JPG; do
+    # Skip if not a regular file (just in case)
+    [[ -f "$file" ]] || continue
 
-	# Build new filename
-	new_name="IMG_${timestamp}.jpg"
+    # Get modification time in desired format
+    timestamp=$(date -r "$file" +"%Y%m%d_%H%M%S")
 
-	# Handle potential name collisions
-	counter=1
-	while [[ -e "$new_name" ]]; do
-		new_name="IMG_${timestamp}_$counter.jpg"
-		((counter++))
-	done
+    # Extract extension and normalize to lowercase
+    ext="${file##*.}"
+    ext="${ext,,}"
 
-	# Rename file
-	mv -v "$file" "$new_name"
+    # Build new filename
+    new_name="IMG_${timestamp}.${ext}"
+
+    # Handle potential name collisions
+    counter=1
+    while [[ -e "$new_name" ]]; do
+        new_name="IMG_${timestamp}_$counter.${ext}"
+        ((counter++))
+    done
+
+    # Rename file
+    mv -v "$file" "$new_name"
 done
